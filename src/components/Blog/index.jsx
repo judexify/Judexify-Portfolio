cd222e55197150de6d7dc0bd89e33c70ba6dc3bd
Judexify <olukojuoluwadunsi12@gmail.com>
2026-03-13 07:45:41 +0100

    feat: implement Blog component with initial content and styling

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -1,3 +1,13 @@
 import './index.css'
 
-export default function Blog() {}
+export default function Blog() {
+  return (
+    <span className="container blog-page">
+      My Blog is coming...
+      <br />
+      I'd be documenting my programming learning journey, what problem i faced,
+      <br />
+      how i solved them. Anticipate!!!
+    </span>
+  )
+}
