024923cdf4fd9980fce2a1176a2c75c92fc0f8d5
Judexify <olukojuoluwadunsi12@gmail.com>
2026-08-20 18:22:59 +0100

    feat: enhance SEO with Open Graph and Twitter meta tags in index.html fix: update logo image source in Logo component

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function Logo() {
   return (
     <div>
-      <Link to='/' className='flex gap-3 items-center'>
-        <img src='/favicon-32x32.png' />
-        <span className='text-xl font-bold text-textColor'>JUDEXIFY.</span>
+      <Link to="/" className="flex gap-3 items-center ">
+        <img src="/pfp.jpeg" height={32} width={32} className="rounded-full" />
+        <span className="text-xl font-bold text-textColor">JUDEXIFY.</span>
       </Link>
     </div>
   );
